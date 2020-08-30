4ebb11b5aa7ade130be70f3de2af7c3d2d90d152
Bharati <akash.bharati@siemens.com>
2020-08-30 19:52:17 +0530

    favico error

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 8080;
 app.use(express.static(__dirname + "/dist/space-x"));
+app.get("/favicon.ico", (req, res) => {
+  res.sendStatus(200);
+});
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/space-x/index.html"));
 });
-app.listen(process.env.PORT || 8080);
+
+app.listen(port, () => {
+  console.log("Server is runnning on ", port);
+});
