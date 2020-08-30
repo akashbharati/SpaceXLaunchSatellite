865f84413081d2d675548edef2378ed77a5e3754
Bharati <akash.bharati@siemens.com>
2020-08-30 20:21:23 +0530

    updates for deploying

--- server.js
+++ server.js
@@ -1,15 +1,15 @@
+//Install express server
 const express = require("express");
 const path = require("path");
+
 const app = express();
-const port = process.env.PORT || 8080;
+
+// Serve only the static files form the dist directory
 app.use(express.static(__dirname + "/dist/space-x"));
-app.get("/favicon.ico", (req, res) => {
-  res.sendStatus(200);
-});
+
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/space-x/index.html"));
 });
 
-app.listen(port, () => {
-  console.log("Server is runnning on ", port);
-});
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
